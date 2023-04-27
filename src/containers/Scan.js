e69b45b06e9f8539942746afa0447e26a398829f
Niels <Nielsfeijen76@gmail.com>
2023-04-27 17:24:43 +0200

    wd

--- src/containers/Scan.js
+++ src/containers/Scan.js
@@ -3,19 +3,74 @@ import Scanner from '../components/Scanner/Scanner';
 import { ActionsContext } from '../contexts/context';
 
 const Scan = () => {
+    const [message, setMessage] = useState('');
+    const [data, setData] = useState('');
+    const [serialNumber, setSerialNumber] = useState('');
     const { actions, setActions } = useContext(ActionsContext);
 
     const scan = useCallback(async () => {
         if ('NDEFReader' in window) {
             console.log("got it")
 
-            setActions({
-                scan: 'scanned',
-            });
+            ndef.onreading = event => {
+                setActions({
+                    scan: 'scanned'
+                });
+            };
 
+            //         setActions({
+            //             scan: 'scanned',
+            //             write: null
+            //         });
+
+            // try {
+            //     const ndef = new window.NDEFReader();
+            //     await ndef.scan();
+
+            //     console.log("Scan started successfully.");
+            //     ndef.onreadingerror = () => {
+            //         console.log("Cannot read data from the NFC tag. Try another one?");
+            //     };
+
+            //     ndef.onreading = event => {
+            //         console.log("NDEF message read.");
+            //         console.log("ndef: "+ ndef.toString())
+            //         console.log("event: "+ event.toString())
+
+            //         onReading(event);
+            //         setData(event.toString())
+            //         setActions({
+            //             scan: 'scanned',
+            //             write: null
+            //         });
+            //     };
+
+            // } catch(error){
+            //     console.log(`Error! Scan failed to start: ${error}.`);
+            // };
         }
     }, [setActions]);
 
+    const onReading = ({ message, serialNumber }) => {
+        setSerialNumber(serialNumber);
+        console.log("message: " + message.toString())
+        console.log("serial: " + serialNumber.toString())
+
+
+        for (const record of message.records) {
+            switch (record.recordType) {
+                case "text":
+                    const textDecoder = new TextDecoder(record.encoding);
+                    setMessage(textDecoder.decode(record.data));
+                    break;
+                case "url":
+                    // TODO: Read URL record with record data.
+                    break;
+                default:
+                // TODO: Handle other records with record data.
+            }
+        }
+    };
 
     useEffect(() => {
         scan();
